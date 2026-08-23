45498571b2b62c9b382c842f0fe8d979acf57a84
Heicel <servitoheicel793@gmail.com>
2026-08-24 02:31:29 +0800

    Added code discussion for Arithmetic, Assignment & Relational Operator

---
+++ src/com/joysistvi/stage1/day3/Activity_Servito.java
@@ -0,0 +1,26 @@
+package com.joysistvi.stage1.day3;
+
+public class Activity_Servito {
+    public static void main(String[] args) {
+
+        boolean isActive = false;
+        char middleInitial = 'C';
+        byte age = 23;
+        short stock = 500;
+        int numberOfCustomers = 800;
+        long population = 1000000;
+        float temp = 38.5F;
+        double decimalNumber = 35.0004;
+        String name = "Heicel Servito";
+
+        System.out.println("(String) Name: " + name);
+        System.out.println("(Byte) Age: " + age);
+        System.out.println("(Short) Stock: " + stock);
+        System.out.println("(Int) No. of Customers: " + numberOfCustomers);
+        System.out.println("(Float) Temp: " + temp);
+        System.out.println("(Double) Decimal: " + decimalNumber);
+        System.out.println("(Char) MiddleInitial: " + middleInitial);
+        System.out.println("(Long) Population: " + population);
+        System.out.println("(Boolean) Statement: " + isActive);
+    }
+}
---
+++ src/com/joysistvi/stage1/day3/VariableThursday.java
@@ -0,0 +1,35 @@
+package com.joysistvi.stage1.day3;
+
+public class VariableThursday {
+
+    public static void main(String[] args) {
+        // initialization
+        String timba = "tubig";
+        int age = 26;
+        String address = "Tondo, Manila";
+
+        // Multiple variables with similar datatypes
+        String progLanguage = "Java", progLanguage2 = "C#", progLanguage3 = "JS";
+
+        System.out.println(progLanguage);
+
+        // declaration
+        float temp;
+
+        // assign value
+        temp = 37.5f;
+
+        System.out.println("Temperature: " + temp);
+        System.out.println("Age: " + age);
+        System.out.println(address + " " + progLanguage);
+
+
+
+        String firstName = "Heicel";
+        String middleInitial = "C";
+        String lastName = "Servito";
+
+        System.out.println("Fullname: " + lastName + " "  + firstName + " " + middleInitial + " ");
+
+    }
+}
