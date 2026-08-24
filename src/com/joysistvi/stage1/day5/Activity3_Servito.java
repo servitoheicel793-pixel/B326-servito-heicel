accc8c39131baf8f8b27c333f0bc3dff960f2d58
Heicel <servitoheicel793@gmail.com>
2026-08-24 13:10:12 +0800

    Added code discussion for Logical, Ternary, Unary Operator

---
+++ src/com/joysistvi/stage1/day5/Activity3_Servito.java
@@ -0,0 +1,13 @@
+package com.joysistvi.stage1.day5;
+
+public class Activity3_Servito {
+    public static void main(String[] args) {
+
+        int age = 18;
+        boolean hasValidId = true;
+
+        String allowed = age >= 18 && hasValidId ? "Yes" : "No";
+        System.out.println("Allowed to enter the event: " + allowed);
+        // Allowed to enter the event : Yes / No
+    }
+}
