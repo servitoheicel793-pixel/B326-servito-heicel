c52ec2a4ea101c16efadeba7a92237be3adef52f
Heicel <servitoheicel793@gmail.com>
2026-09-07 18:56:23 +0800

    Added code discussion for Activity11MathClass, MathClass, Function, Laptop, Main, LoadRegistration, Main, Scope

---
+++ day12/Activity11MathClass_Servito.java
@@ -0,0 +1,23 @@
+package com.joysistvi.stage1.day12;
+
+public class Activity11MathClass_Servito {
+
+    public static void main(String[] args) {
+
+        System.out.println("abs: " + Math.abs(-10));
+        System.out.println("max: " + Math.max(10, 20));
+        System.out.println("min: " + Math.min(10, 20));
+        System.out.println("pow: " + Math.pow(2, 3));
+        System.out.println("sqrt: " + Math.sqrt(25));
+        System.out.println("round: " + Math.round(5.6));
+        System.out.println("ceil: " + Math.ceil(5.2));
+        System.out.println("floor: " + Math.floor(5.8));
+        System.out.println("random: " + Math.random());
+        System.out.println("log: " + Math.log(10));
+        System.out.println("log10: " + Math.log10(100));
+        System.out.println("exp: " + Math.exp(2));
+        System.out.println("sin: " + Math.sin(Math.PI / 2));
+        System.out.println("cos: " + Math.cos(0));
+        System.out.println("tan: " + Math.tan(Math.PI / 4));
+    }
+}
